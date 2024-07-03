72816117b52849e73a3382869a28db40c8b7279a
Shashwat Raj <shashwatraj30@gmail.com>
2024-07-03 11:06:33 +0530

    route processed

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const MongoStore = require("connect-mongo")(session);
 const passport = require("passport");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 connectDb();
 
